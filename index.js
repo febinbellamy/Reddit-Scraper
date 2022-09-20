b4dfdc576ca871bf7b2af1556b84403230bab2a8
Febin Bellamy <febinbellamy@gmail.com>
2022-09-20 17:25:21 -0400

    Final updates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const Sheet = require("./sheet");
 
 const url =
   "https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/";
@@ -8,16 +9,26 @@ const url =
   const page = await browser.newPage();
   await page.goto(url);
 
+  const sheet = new Sheet();
+  await sheet.load();
+
+  // create sheet with title
+  const title = await page.$eval(".title a", (elem) => elem.textContent);
+  const sheetIndex = await sheet.addSheet(title.slice(0, 99), [
+    "points",
+    "text",
+  ]);
+
   // expand all comment threads:
-  // let expandButtons = await page.$$(".morecomments");
-  // while (expandButtons.length) {
-  //   for (let button of expandButtons) {
-  //     await button.click();
-  //     await page.waitForTimeout(500);
-  //   }
-  //   await page.waitForTimeout(1000);
-  //   expandButtons = await page.$$(".morecomments");
-  // }
+  let expandButtons = await page.$$(".morecomments");
+  while (expandButtons.length) {
+    for (let button of expandButtons) {
+      await button.click();
+      await page.waitForTimeout(500);
+    }
+    await page.waitForTimeout(1000);
+    expandButtons = await page.$$(".morecomments");
+  }
 
   // for every comment, scrape the text and the points
   const comments = await page.$$(".entry");
@@ -48,6 +59,6 @@ const url =
   console.log(formattedComments.slice(0, 10));
 
   // insert the sorted comments into the google spreadsheet
-  
-  // await browser.close();
+  sheet.addRows(formattedComments, sheetIndex);
+  await browser.close();
 })();
--- sheet.js
+++ sheet.js
@@ -6,10 +6,17 @@ module.exports = class Sheet {
       "1RxRawgrexPBPUx7S5nkXoT_lgDdOVmVNjMYBmTx6SAA"
     );
   }
+
   async load() {
     await this.doc.useServiceAccountAuth(require("./credentials.json"));
     await this.doc.loadInfo();
   }
+
+  async addSheet(title, headerValues) {
+    await this.doc.addSheet({ title, headerValues });
+    return this.doc.sheetsByIndex.length - 1;
+  }
+
   async addRows(rows, i) {
     const sheet = this.doc.sheetsByIndex[i];
     await sheet.addRows(rows);
